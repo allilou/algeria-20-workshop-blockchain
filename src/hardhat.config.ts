c9c8ab41ff19ec2424e8195776d429a6f0b61b85
Ali BENAHMED DAHO <ali.benahmeddaho@transformatek.dz>
2024-11-19 23:47:34 +0000

    Add deploy with hardhat-web3js

--- src/hardhat.config.ts
+++ src/hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import '@nomicfoundation/hardhat-toolbox';
+import '@nomicfoundation/hardhat-web3-v4';
 
 const config: HardhatUserConfig = {
   solidity: "0.8.27",
